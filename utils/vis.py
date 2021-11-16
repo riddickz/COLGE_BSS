b19803c9b5a7893bac7f1ed03650afe43eca91ef
Riddick Zhou <jiamingzhou118@gmail.com>
2021-11-16 00:35:20 -0500

    update for plot

--- utils/vis.py
+++ utils/vis.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 def visualize_2D(nodes, W, nodes_weight=None): # Plot tour
     plt.figure(figsize=(20,15))
@@ -29,3 +31,30 @@ def visualize_2D(nodes, W, nodes_weight=None): # Plot tour
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.show()
+
+def plot_reward(reward_list):
+    plt.ion()
+    plt.figure(1)
+    window = 5 #int(max_episodes/20)
+    fig, ((ax1), (ax2)) = plt.subplots(2, 1, sharey=True, figsize=[9,9])
+    rolling_mean = pd.Series(reward_list).rolling(window).mean()
+
+    std = pd.Series(reward_list).rolling(window).std()
+    ax1.plot(rolling_mean)
+    ax1.fill_between(range(len(reward_list)),rolling_mean-std, rolling_mean+std, color='orange', alpha=0.2)
+    ax1.set_title('Episode Length Moving Average ({}-episode window)'.format(window))
+    ax1.set_xlabel('Episode'); ax1.set_ylabel('Episode Length')
+    plt.grid()
+
+    x = np.arange(len(reward_list))
+    ax2.scatter(x, reward_list, s=1.3)
+    ax2.plot(reward_list,alpha=0.3, linewidth=0.8)
+    ax2.set_title('Episode Length')
+    ax2.set_xlabel('Episode'); ax2.set_ylabel('Episode Length')
+
+    ax1.grid()
+    ax2.grid()
+    fig.tight_layout(pad=2)
+    # plt.show()
+    plt.pause(0.001)
+    plt.close()
